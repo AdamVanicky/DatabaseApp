79705aaafea1d33792be659e9d885005049055ff
Vanický Adam <vanicky.adam.2017@skola.ssps.cz>
2021-01-26 16:31:55 +0100

    Přidejte soubory projektu.

---
+++ DatabaseApp/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DatabaseApp
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void butAddPerson_Click(object sender, RoutedEventArgs e)
+        {
+            Person p = new Person(tbFirstName.Text, tbSurName.Text, tbPersonNumber.Text, dtDateofBirth.DisplayDate);
+            Database d = Database.CreateOne;
+            
+            MessageBox.Show($"{d}");
+        }
+    }
+
+    class Database
+    {
+        static Dictionary<string, Person> databaze;
+        private static readonly object locking = new object();
+        static Database instance = null;
+
+        private Database()
+        {
+            databaze = new Dictionary<string, Person>();
+        }
+
+        public static Database CreateOne
+        {
+            get 
+            { 
+                lock(locking)
+                {
+                    if(instance == null)
+                    {
+                        instance = new Database();
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
---
+++ DatabaseApp/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseApp
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string RodneCislo { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Person(string firstname, string surname, string rodnecislo, DateTime dateofbirth)
+        {
+            FirstName = firstname;
+            SurName = surname;
+            RodneCislo = rodnecislo;
+            DateOfBirth = dateofbirth;
+        }
+    }
+}
